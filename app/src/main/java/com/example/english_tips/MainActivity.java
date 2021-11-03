9745b4ae5931fe9634d7990c8a34fac30d9de8b9
Aleksey Dolgov <noizeroobots@gmail.com>
2021-11-04 00:05:34 +0300

    Итоги по 3 уроку

--- app/src/main/java/com/example/english_tips/MainActivity.java
+++ app/src/main/java/com/example/english_tips/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.english_tips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
+import com.example.english_tips.adapter.CategoryAdapter;
+import com.example.english_tips.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "November"));
+        categoryList.add(new Category(2, "December"));
+        categoryList.add(new Category(3, "January"));
+        categoryList.add(new Category(4, "February"));
+        categoryList.add(new Category(5, "March"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+
+
     }
 }
